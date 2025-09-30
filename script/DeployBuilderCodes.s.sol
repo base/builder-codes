1dded1be998ea54df79a2263d586a0253ce6d9ad
Conner Swenberg <conner.swenberg@coinbase.com>
2025-09-30 11:47:43 -0700

    Update deploy script with CREATE2 salts

--- script/DeployBuilderCodes.s.sol
+++ script/DeployBuilderCodes.s.sol
@@ -10,7 +10,7 @@ import {BuilderCodes} from "../src/BuilderCodes.sol";
 /// @notice Script for deploying the BuilderCodes contract
 contract DeployBuilderCodes is Script {
     function run() external returns (address) {
-        address owner = 0x0000000000000000000000000000000000000000;
+        address owner = 0x1D8958f7b9AE9FbB9d78C1e1aB18b44Fd54a0B7A; // testnet key
         address initialRegistrar = 0x0000000000000000000000000000000000000000;
         string memory uriPrefix = "https://flywheel.com/";
 
@@ -20,13 +20,14 @@ contract DeployBuilderCodes is Script {
         vm.startBroadcast();
 
         // Deploy the implementation contract
-        BuilderCodes implementation = new BuilderCodes();
+        BuilderCodes implementation = new BuilderCodes{salt: 0}();
 
         // Prepare initialization data
         bytes memory initData = abi.encodeCall(BuilderCodes.initialize, (owner, initialRegistrar, uriPrefix));
+        console.logBytes(initData);
 
         // Deploy the proxy
-        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), initData);
+        ERC1967Proxy proxy = new ERC1967Proxy{salt: 0}(address(implementation), initData);
 
         console.log("BuilderCodes implementation deployed at:", address(implementation));
         console.log("BuilderCodes proxy deployed at:", address(proxy));
