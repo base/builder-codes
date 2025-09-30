bd01de36ab9d36486483f2e5c5de4eac861578a6
Conner Swenberg <conner.swenberg@coinbase.com>
2025-09-30 11:56:51 -0700

    Add initial registrar

--- script/DeployBuilderCodes.s.sol
+++ script/DeployBuilderCodes.s.sol
@@ -11,7 +11,7 @@ import {BuilderCodes} from "../src/BuilderCodes.sol";
 contract DeployBuilderCodes is Script {
     function run() external returns (address) {
         address owner = 0x1D8958f7b9AE9FbB9d78C1e1aB18b44Fd54a0B7A; // testnet key
-        address initialRegistrar = 0x0000000000000000000000000000000000000000;
+        address initialRegistrar = 0x6Bd08aCF2f8839eAa8a2443601F2DeED892cd389; // dev registrar key
         string memory uriPrefix = "https://flywheel.com/";
 
         console.log("Initial registrar:", initialRegistrar);
