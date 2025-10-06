7e53f85e0813e93776fcaef1b8cb5c36927d86f2
Conner Swenberg <conner.swenberg@coinbase.com>
2025-10-06 10:51:17 -0500

    Add build-info to build-production command (#12)

--- script/DeployBuilderCodes.s.sol
+++ script/DeployBuilderCodes.s.sol
@@ -10,7 +10,9 @@ import {BuilderCodes} from "../src/BuilderCodes.sol";
 /// @notice Script for deploying the BuilderCodes contract
 contract DeployBuilderCodes is Script {
     function run() external returns (address) {
-        address owner = 0x1D8958f7b9AE9FbB9d78C1e1aB18b44Fd54a0B7A; // testnet key
+        // address owner = 0x0BFc799dF7e440b7C88cC2454f12C58f8a29D986; // local key
+        // address initialRegistrar = 0x4175fad66ebB1240dff55e018830C61Dd646FCce; // dev registrar key
+        address owner = 0x1D8958f7b9AE9FbB9d78C1e1aB18b44Fd54a0B7A; // dev testnet key
         address initialRegistrar = 0x6Bd08aCF2f8839eAa8a2443601F2DeED892cd389; // dev registrar key
         string memory uriPrefix = "https://api-spindl.coinbase.com/flywheel/metadata/nft/";
 
